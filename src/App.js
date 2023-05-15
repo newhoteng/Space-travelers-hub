e409b998a8eddf630be7e935322dc76818f433ae
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-15 18:06:25 +0000

    Make nav links work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Missions />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          {/* <Route index element={<Rockets />} /> */}
+          {/* <Route path="/dragons" element={<Dragons />} /> */}
+          <Route path="/missions" element={<Missions />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/*" element={<div>Page not found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <Header />
+    //   <Missions />
+    // </div>
   );
 }
 
