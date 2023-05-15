ff9d5592d4019dfb33c69d814ad796d79e773f33
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-15 23:54:17 +0000

    Add mission joining actions

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { joinMission } from '../redux/missions/missionsSlice';
 import styles from '../styles/Missions.module.css';
 
 function MissionItem({ itemProps }) {
+  const dispatch = useDispatch();
   return (
     <tr className={styles.tablerow}>
       <td>{itemProps.name}</td>
       <td>{itemProps.description}</td>
-      <td><button className={styles.member} type="button">NOT A MEMBER</button></td>
-      <td><button className={styles.join} type="button">Join Mission</button></td>
+      <td>
+        {!itemProps.reserved && (
+          <button className={`${styles.nonMember} ${styles.status}`} type="button">NOT A MEMBER</button>
+        )}
+        {itemProps.reserved && (
+          <button className={`${styles.member} ${styles.status}`} type="button">Active Member</button>
+        )}
+      </td>
+      <td>
+        {!itemProps.reserved && (
+          <button
+            className={`${styles.join} ${styles.joinLeave}`}
+            type="button"
+            onClick={() => { dispatch(joinMission(itemProps.id)); }}
+          >
+            Join Mission
+          </button>
+        )}
+        {itemProps.reserved && (
+          <button className={`${styles.leave} ${styles.joinLeave}`} type="button">Leave Mission</button>
+        )}
+      </td>
     </tr>
   );
 }
@@ -20,5 +43,6 @@ MissionItem.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    reserved: PropTypes.bool.isRequired,
   }).isRequired,
 };
