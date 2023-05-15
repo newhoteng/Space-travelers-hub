5fa304c53f7107b0deebd11c4e469914f7e0e653
Oumaima Nejjari <oumanj1998@gmail.com>
2023-05-15 20:21:18 +0100

    Createroutes and directory for Redux Slices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Missions from './pages/MissionsPage';
 import MyProfile from './pages/MyProfilePage';
 import Rockets from './pages/RocketsPage';
@@ -7,7 +7,7 @@ import Rockets from './pages/RocketsPage';
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
       <div className="container">
         <Routes>
           <Route path="/rockets" element={<Rockets />} />
