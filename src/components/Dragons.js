95e67cf92ae28c2e64a30ec9779509f2a9226310
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-05-16 12:24:51 +0300

    Add Dragons layout

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDragons, reserveDragon } from '../redux/Dragons/DragonsSlice';
 
-function Dragons() {
+const Dragons = () => {
+  const dispatch = useDispatch();
+  const { dragon, status } = useSelector((store) => store.dragon);
+
+  const [reserved, setReserved] = useState(false);
+
+  useEffect(() => {
+    if (status === false) dispatch(getDragons());
+  }, [dispatch, status]);
+
+  const handleReserveDragon = (id) => {
+    dispatch(reserveDragon(id));
+    setReserved(!reserved);
+  };
+
   return (
-    <div>Dragons</div>
+    <div className="dragons-container">
+      {status
+        && dragon.map((dragon) => (
+          <div className="container" key={dragon.id}>
+            <div className="article">
+              <img
+                src={dragon.image}
+                alt={dragon.name}
+                className="dragon-image"
+              />
+            </div>
+            <div className="desc">
+              <h2 className="dragon-title">{dragon.name}</h2>
+              <p className="dragon-details">
+                {reserved && <span className="reserved">Reserved</span>}
+                {dragon.description}
+              </p>
+              <button
+                type="button"
+                className={reserved ? 'cancel' : 'reserve'}
+                onClick={() => handleReserveDragon(dragon.id)}
+              >
+                {reserved ? 'Cancel Reservation' : 'Reserve Dragon'}
+              </button>
+            </div>
+          </div>
+        ))}
+    </div>
   );
-}
+};
 
 export default Dragons;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionsSlice';
+import dragonReducer from './Dragons/DragonsSlice';
 
 export const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    dragon: dragonReducer,
   },
 });
 
